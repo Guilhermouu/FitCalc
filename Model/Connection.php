716fc1957b010a73c71535c01f969caa63fe6cd2
Guilhermouu <guilhermo4t2l@gmail.com>
2025-06-17 16:39:48 -0300

    feat: Instalação do Composer/autoload _ PDO

---
+++ Model/Connection.php
@@ -0,0 +1,22 @@
+<?php
+namespace Model;
+
+require_once __DIR__. '/../Config/configuration.php';
+
+use PDO;
+use PDOException;   
+class Connection{
+    private $stmt;
+    
+    private static function getInstance(): PDO{  
+        if(empty(self::$stmt)){
+            try{
+                self::$stmt = new PDO('mysql:host='. DB_HOST. ';port'.DB_PORT . ';dbname='. DB_NAME .'', DB_USER, DB_PASSWORD);
+    }catch(PDOException $error){
+        die($error ->getMessage());
+    }
+  }
+  return self::$stmt;
+ }
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -7,11 +11,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../templates/assets/css/register.css">
-    <title>FitCalc | Criar Conta</title>
+    <link rel="stylesheet" href="templates/assets/css/login.css">
+    <title>FitCalc | Entrar na Conta</title>
+
 </head>
 
 <body class="bgLinearGradient">
+
     <div vw class="enabled">
         <div vw-access-button class="active"></div>
         <div vw-plugin-wrapper>
@@ -24,27 +30,22 @@
     </script>
 
     <main class="d-flex justify-content-center align-items-center flex-column h-100">
-        <form method="POST" class="bg-light p-5 rounded-4 w-25">
+        <form method="POST" class="bg-light rounded-4">
             <div class="form__create-account d-flex flex-column justify-content-center align-items-center">
-                <figure class="rounded-circle d-flex justify-content-center align-items-center bgLinearGradient">
-                    <i class="bi bi-calculator-fill fs-5 text-white"></i>
+                <figure class="bgLinearGradient rounded-circle d-flex justify-content-center align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-calculator"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+                        <path
+                            d="M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
+                    </svg>
                 </figure>
 
-                <h2 class="fw-bold">Criar Conta</h2>
+                <h2 class="fw-bold">Calculadora IMC</h2>
 
-                <p>Preencha seus dados</p>
+                <p>Entre com suas credenciais</p>
 
-            </div>
-
-            <div class="mb-3 position-relative">
-                <label for="userName" class="form-label">Nome completo</label>
-                <div class="input-group">
-                    <span class="input-group-text bg-white border-end-0">
-                        <i class="bi bi-person"></i>
-                    </span>
-                    <input type="text" name="user_fullname" class="form-control border-start-0 inputPlaceholder p-2"
-                        id="userName" placeholder="Seu nome completo" aria-describedby="userName">
-                </div>
             </div>
 
             <div class="mb-3 position-relative">
@@ -54,7 +55,7 @@
                         <i class="bi bi-envelope"></i>
                     </span>
                     <input type="email" name="email" class="form-control border-start-0 inputPlaceholder p-2"
-                        id="userEmailAddress" placeholder="seu@email.com" aria-describedby="emailAddress">
+                        id="userEmailAddress" placeholder="seu@email.com" aria-describedby="emailAddress" required>
                 </div>
             </div>
 
@@ -65,28 +66,16 @@
                         <i class="bi bi-lock"></i>
                     </span>
                     <input type="password" name="password" class="form-control border-start-0 inputPlaceholder p-2"
-                        id="userPassword" placeholder="Sua senha">
+                        id="userPassword" placeholder="Sua senha" required>
                 </div>
             </div>
 
-            <div class="mb-3 position-relative">
-                <label for="userConfirmPassword" class="form-label">Confirmar senha</label>
-                <div class="input-group">
-                    <span class="input-group-text bg-white border-end-0">
-                        <i class="bi bi-lock"></i>
-                    </span>
-                    <input type="password" name="userConfirmPassword"
-                        class="form-control border-start-0 inputPlaceholder p-2" id="userConfirmPassword"
-                        placeholder="Confirme sua senha">
-                </div>
-            </div>
+            <button type="submit" class="bgLinearGradient rounded-3 w-100 mb-4">Entrar</button>
 
-            <button type="submit"
-                class="border-0 text-white rounded-3 w-100 mb-4 p-2 bgLinearGradient">Cadastrar</button>
-
-            <p class="text-center">Já tem uma conta? <a href="../index.php">Faça login aqui</a></p>
+            <p class="text-center">Não tem uma conta? <a href="View/register.php">Cadastre-se aqui</a></p>
             </div>
         </form>
+
         <p></p>
     </main>
 
